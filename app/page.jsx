83567f7cdd105885acf6d587e4380c46165798d9
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-17 21:56:01 +0530

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        
+      </div>
+    </main>
+  );
+}
