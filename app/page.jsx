321bbdda6ce35e5f177c3586f4d805be96ac5939
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-17 22:08:04 +0530

    Added Navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        
+    <main className="relative flex bg-[#000000] justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full ">
+        <Navbar />
       </div>
     </main>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='flex items-center justify-between'>
+      <div className='pt-[60px]'>
+        <img src="/logo-no-background.svg" height={80} width={80} alt="" />
+      </div>
+      <div className='pt-[90px]'>
+        <img src="/hamburger.svg" height={24} width={36} alt="" />
+      </div>
     </div>
   )
 }
