9b13ae5d2586b07c4d1632f9e0581f6b99f612d8
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-19 15:24:53 +0530

    Added marquee animation

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -17,15 +17,27 @@ const Hero = () => {
     tl.staggerTo(spans, 0.6, { opacity: 1, y: 0, rotation: 360, scale: 1.2 }, 0.1) // Stagger for each span
       .to(spans, { rotation: 0, scale: 1, ease: "elastic.out(1, 0.3)", stagger: 0.1 }) // Reset animation
 
+      // marquee animation
+      const marqueeText = document.querySelector(".marquee-animation p");
+      const marqueeAnimation = gsap.to(marqueeText, {
+        xPercent: 100,
+        repeat: -1,
+        duration: 5,
+        ease: "linear"
+      })
+
   }, []);
 
   return (
-    <div className='hero-section container mx-auto'>
+    <div className='hero-section container mx-auto relative'>
       <div className='text-white text-center m-auto text-[96px] mt-[195px]'>
         <h1 className='opacity-1'><span>R</span><span>I</span><span>Z</span><span>W</span><span>A</span><span>A</span><span>N</span> <span>A</span><span>N</span><span>S</span><span>A</span><span>R</span><span>I</span></h1>
       </div>
       <div className='text-white text-center m-auto text-[32px] leading-[0px] pb-[335px] tracking-[6px]'>
         <p>FULL STACK DEVELOPER</p>
+      </div>
+      <div className='marquee-animation text-white text-[2rem] overflow-hidden whitespace-nowrap absolute bottom-20 right-0 transform -rotate-12'>
+      <p className='inline-block'>NEXT JS REACT JS NODE JS PHP DOCKER &nbsp;</p>
       </div>
     </div>
   );
