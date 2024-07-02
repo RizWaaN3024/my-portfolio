362f9f4da76d9062ca2c38e2cf322d8c92336abf
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-02 23:43:32 +0530

    Hero Section Fixes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
   // }, []);
 
   return (
-    <div className={`hero-section container mx-auto ${spartan.className} py-[100px]`}>
+    <div className={`hero-section container mx-auto ${spartan.className} py-[100px] border-b border-[#484E53]`}>
       {/* <div className='text-white text-center m-auto text-[96px] mt-[195px]'>
         <h1 className='opacity-1'><span>R</span><span>I</span><span>Z</span><span>W</span><span>A</span><span>A</span><span>N</span> <span>A</span><span>N</span><span>S</span><span>A</span><span>R</span><span>I</span></h1>
       </div>
